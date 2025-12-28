05d3357a8ec03689ac54708f71814808a783b15d
venith R <venithraja@ezovion.com>
2025-12-28 09:53:51 +0530

    service model

--- src/model/servicesModel.js
+++ src/model/servicesModel.js
@@ -1,29 +1,55 @@
 const mongoose = require("mongoose");
 
-const serviceSchema = new mongoose.Schema(
-    {
-        _id: { type: mongoose.Schema.Types.ObjectId, auto: true }, // auto-generates id
-        name: { type: String, required: true },
-        price: { type: Number, required: true },
-        discount: { type: Number, required: false },
-        serviceType: {
-            type: String,
-            enum: ["weekday", "weekend"],
-            required: true,
-        },
+const mongoose = require("mongoose");
 
+const serviceSchema = new mongoose.Schema(
+  {
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+
+    serviceId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
-    { timestamps: true }
+
+    name: { type: String, required: true },
+
+    weekdayPrice: { type: Number, required: true },
+    weekendPrice: { type: Number, required: true },
+
+    finalWeekdayPrice: { type: Number, required: true },
+    finalWeekendPrice: { type: Number, required: true },
+
+    weekdayDiscount: { type: Number, default: 0 },
+    weekdayDiscountType: {
+      type: String,
+      enum: ["Percentage", "Flat"],
+      default: "Percentage",
+    },
+
+    weekendDiscount: { type: Number, default: 0 },
+    weekendDiscountType: {
+      type: String,
+      enum: ["Percentage", "Flat"],
+      default: "Percentage",
+    },
+  },
+  { timestamps: true }
 );
 
-const categorySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    businessOwnerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "userSchema",
-        required: false,
-    },
-    services: [serviceSchema],
-});
 
-module.exports = mongoose.model("Services", categorySchema,"Services");
+const categorySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+
+    businessOwnerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+
+    services: [serviceSchema],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Services", categorySchema, "Services");
